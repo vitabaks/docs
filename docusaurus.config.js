5e336adc295d455f48379bcbd198caf41fa5b2f4
Vitaliy Kukharik <vitabaks@gmail.com>
2024-08-23 13:22:21 +0300

    update image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/postgresql_cluster.png',
       navbar: {
         title: 'PostgreSQL Cluster\ndocumentation',
         logo: {
