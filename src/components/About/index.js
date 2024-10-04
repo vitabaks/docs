2f60c41860ecc2f9d16283afd5aafab66f8834a0
Vitaliy Kukharik <vitabaks@gmail.com>
2024-10-04 14:22:56 +0300

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -22,7 +22,7 @@ export default function About() {
         <a href={postgresqlLink} target="_blank" rel="noopener noreferrer">PostgreSQL</a> is a powerful, open source object-relational database system with over 35 years of active development, that has earned it a strong reputation for reliability, feature robustness, and performance.
         More and more companies around the world are choosing it for their new products, or migrating from other database systems to PostgreSQL. 
         It has been ranked as the <a href={dbmsOfYearLink} target="_blank" rel="noopener noreferrer">DBMS of the Year</a> multiple times, and its popularity continues to grow, as illustrated by the 
-        <a href={dbEnginesTrendLink} target="_blank" rel="noopener noreferrer">DB-Engines ranking trend:</a>
+        <a href={dbEnginesTrendLink} target="_blank" rel="noopener noreferrer"> DB-Engines ranking trend:</a>
       </p>
       <img src="/img/db-engines-ranking.png" alt="DB-Engines Ranking Trend" className={styles.thumbnail} onClick={toggleModal} />
       {isModalOpen && (
@@ -36,11 +36,11 @@ export default function About() {
       <p className={styles.description}>
         PostgreSQL is an outstanding database; however, unlike commercial DBMS solutions, it doesn’t include built-in tools for high availability, monitoring dashboards, or a graphical management console commonly found in commercial offerings.
         While this might be seen as a drawback, it can also be an advantage, since the open-source world offers various ways to tackle these tasks.
-        There are numerous open-source tools available, but combining them into a reliable, enterprise-ready database cluster requires solid experience with PostgreSQL and these tools to create a truly dependable solution ready for production.
+        There are numerous open-source tools available, but combining them into a reliable, Enterprise-ready database cluster requires solid experience with PostgreSQL and these tools to create a truly dependable solution ready for production.
       </p>
       <p className={styles.description}>
         To bridge this gap, the <a href={postgresqlClusterLink} target="_blank" rel="noopener noreferrer">postgresql_cluster</a> automation toolkit was developed in 2019 for personal use, and later shared with the community.
-        It handles the deployment of high-availability clusters (using Patroni) and addresses management tasks such as minor and major upgrades, backup configuration with support for automated cluster recovery to any point in time (PITR), and cluster scaling with horizontal read load distribution across replicas, among many other features.
+        It handles the deployment of high-availability clusters (using Patroni) and addresses management tasks such as minor and major upgrades, backup configuration with support for automated restore a database to a point in time (PITR), and cluster scaling with horizontal read load distribution across replicas, among many other features.
         The project has been around for over 5 years and is used by companies worldwide. It continues to actively evolve and improve, leveraging the practical experience of PostgreSQL experts (DBA) who utilize our product to manage database clusters in production environments for mission-critical, high-load databases.
       </p>
       <p className={styles.description}>
