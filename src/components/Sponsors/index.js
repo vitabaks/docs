7e615f6a40c3dfbd051f599c30952de908b1fe06
Vitaliy Kukharik <vitabaks@gmail.com>
2024-10-22 23:18:32 +0300

    Update index.js

--- src/components/Sponsors/index.js
+++ src/components/Sponsors/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useColorMode } from '@docusaurus/theme-common';
 import styles from './styles.module.css';
 
@@ -7,25 +7,31 @@ const Sponsors = [
     name: 'edclub',
     lightImage: '/img/sponsors/edclub.png',
     darkImage: '/img/sponsors/edclub.dark.png',
-    link: 'https://www.edclub.com'
+    link: 'https://www.edclub.com',
   },
   {
     name: 'gs-labs',
     lightImage: '/img/sponsors/gs-labs.png',
     darkImage: '/img/sponsors/gs-labs.dark.png',
-    link: 'https://gs-labs.ru'
+    link: 'https://gs-labs.ru',
   },
   {
     name: 'Postgres.AI',
     lightImage: '/img/sponsors/postgresai.png',
     darkImage: '/img/sponsors/postgresai.dark.png',
-    link: 'https://postgres.ai'
+    link: 'https://postgres.ai',
   },
 ];
 
 function Sponsor({ name, lightImage, darkImage, link }) {
   const { colorMode } = useColorMode();
-  const image = colorMode === 'dark' ? darkImage : lightImage;
+  const [currentMode, setCurrentMode] = useState(colorMode);
+
+  useEffect(() => {
+    setCurrentMode(colorMode);
+  }, [colorMode]);
+
+  const image = currentMode === 'dark' ? darkImage : lightImage;
 
   return (
     <div className={styles.Sponsor} style={{ marginBottom: '30px' }}>
