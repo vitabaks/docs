3c596b2afbcafc3aa845236b69d5f4d1bdb2f179
Vitaliy Kukharik <vitabaks@gmail.com>
2024-11-13 14:32:07 +0300

    Add social card image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      //image: 'img/postgresql_cluster.png',
+      image: 'img/pg-console.png',
       /*
       announcementBar: {
         content:
