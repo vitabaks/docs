de9a4ceb744624175828474081e9479068aa578f
Vitaliy Kukharik <vitabaks@gmail.com>
2024-11-28 19:02:18 +0300

    autobase social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/autobase-logo.png',
+      image: 'img/autobase-social-card.png',
       /*
       announcementBar: {
         content:
