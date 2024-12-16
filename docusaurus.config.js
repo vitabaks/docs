c71b676428c67f22e158da2e44b4c0dbcdf268d1
Vitaliy Kukharik <vitabaks@gmail.com>
2024-12-16 22:56:33 +0500

    announcementBar: autobase version 2.1.0 is out!

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,16 +74,14 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/autobase-social-card.png',
-      /*
       announcementBar: {
         content:
-          //'🎉️ <a target="_blank" href="https://github.com/vitabaks/autobase/releases/tag/2.0.0">autobase v2.0</a> is out! 🥳️',
+          '🎉️ <b><a target="_blank" href="https://github.com/vitabaks/autobase/releases/tag/2.1.0">autobase version 2.1.0 is out!</a></b> 🥳️',
           // '⭐️ If you like <b>autobase</b>, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/vitabaks/autobase">GitHub</a>! ⭐️',
           backgroundColor: '#F6F8FA',
           textColor: '#091E42',
           isCloseable: true,
       },
-      */
       navbar: {
         title: 'autobase',
         logo: {
