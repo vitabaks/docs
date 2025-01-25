a50cc1d549660fcb489b97c5f986863cc80e0e67
Vitaliy Kukharik <vitabaks@gmail.com>
2025-01-25 22:08:29 +0700

    Your self-hosted DBaaS

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,11 +18,11 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h2" style={{ fontSize: '2.7rem', fontWeight: '400' }}>
-          Automated database platform for PostgreSQL®
+        <Heading as="h2" style={{ fontSize: '2.4rem', fontWeight: '400' }}>
+          Autobase for PostgreSQL® - Your self-hosted DBaaS
         </Heading>
-        <Heading as="h3" style={{ fontSize: '1.2rem', fontWeight: '400', marginBottom: '40px' }}>
-          Open source alternative to cloud-managed databases (your own self-hosted DBaaS)
+        <Heading as="h3" style={{ fontSize: '1.4rem', fontWeight: '400', marginBottom: '40px' }}>
+          Open source alternative to cloud-managed databases
         </Heading>
         <div className={styles.buttons}>
           <Link
