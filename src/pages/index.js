47126cc0107de62fadc6d785024032ff86c449b7
Vitaliy Kukharik <vitabaks@gmail.com>
2025-01-29 00:29:12 +0700

    Update description

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,12 +61,9 @@ function HomepageHeader() {
         <Heading as="h2" style={{ fontSize: '2.6rem', fontWeight: '600' }} className={styles.gradientText}>
           Autobase for PostgreSQL® - Your Self-Hosted DBaaS
         </Heading>
-        <Heading as="h3" style={{ fontSize: '1.4rem', fontWeight: '400' }}>
-          Open-source alternative to cloud-managed databases
-        </Heading>
-        <Heading as="h3" style={{ fontSize: '1.4rem', fontWeight: '400', marginTop: '40px', marginBottom: '40px' }} className={styles.heroBanner_description}>
-          Autobase is a comprehensive Database-as-a-Service (DBaaS) solution for deploying and managing PostgreSQL databases,
-          designed for database administrators (DBAs) and developers.
+        <Heading as="h3" style={{ fontSize: '1.4rem', fontWeight: '400', marginTop: '20px', marginBottom: '40px' }} className={styles.heroBanner_description}>
+          Autobase is a comprehensive Database-as-a-Service (DBaaS) solution for easy and efficient deployment and management of PostgreSQL databases.
+          It is designed for IT specialists, including system administrators (SysAdmins), site reliability engineers (SREs), database reliability engineers (DBREs), DevOps engineers, database administrators (DBAs), and developers.
         </Heading>
         <div className={styles.buttons}>
           <Link
