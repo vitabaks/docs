dcc046f9b195498ed0f840bd27d2274251a8fec3
Vitaliy Kukharik <vitabaks@gmail.com>
2025-11-24 22:41:29 +0700

    Add Postgres 18 in Extensions Table

--- src/components/ExtensionsTable/index.js
+++ src/components/ExtensionsTable/index.js
@@ -8,7 +8,7 @@ const ExtensionsTable = () => {
   const [categories, setCategories] = useState([]);
   const [repos, setRepos] = useState([]);
   const [categoryFilter, setCategoryFilter] = useState('ALL');
-  const [versionFilter, setVersionFilter] = useState('17');
+  const [versionFilter, setVersionFilter] = useState('18');
   const [typeFilter, setTypeFilter] = useState('DEB');
   const [repoFilter, setRepoFilter] = useState('ALL');
   const [searchQuery, setSearchQuery] = useState('');
@@ -25,7 +25,7 @@ const ExtensionsTable = () => {
         setData(data);
         setCategories(getUniqueCategories(data));
         setRepos(getUniqueRepos(data, 'DEB'));
-        applyFilters(data, 'ALL', '17', 'DEB', 'ALL', '');
+        applyFilters(data, 'ALL', '18', 'DEB', 'ALL', '');
       },
       error: (error) => {
         console.error('Error loading CSV:', error);
@@ -129,12 +129,12 @@ const ExtensionsTable = () => {
         <label style={{ marginLeft: '10px' }}>
           Postgres Version:{' '}
           <select value={versionFilter} onChange={handleVersionChange}>
+            <option value="18">18</option>
             <option value="17">17</option>
             <option value="16">16</option>
             <option value="15">15</option>
             <option value="14">14</option>
             <option value="13">13</option>
-            <option value="12">12</option>
           </select>
         </label>
 
