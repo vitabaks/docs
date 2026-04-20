16caf6f51b25ab6c91a5f0867707b9013b16d904
Vitaliy Kukharik <vitabaks@gmail.com>
2026-04-20 23:38:01 +0200

    Over 500 extensions are available

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -74,7 +74,7 @@ const blocks = [
 		description: (
 			<>
 				PostgreSQL is highly extensible.
-				With Autobase, you can easily install any of the 400+ available <a href="/docs/extensions/list" target="_blank" rel="noopener noreferrer">extensions </a>
+				With Autobase, you can easily install any of the 500+ available <a href="/docs/extensions/list" target="_blank" rel="noopener noreferrer">extensions </a>
 				to equip your database with powerful capabilities tailored to your project’s unique requirements.
 			</>
 		),
