65d245915497403ea5a2faea81812c738c5cf370
Vitaliy Kukharik <vitabaks@gmail.com>
2026-08-14 17:19:42 +0200

    disable announcement bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,12 +78,14 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/autobase-social-card.png',
+      /*
       announcementBar: {
         content:
           '<span class="announcement-system">[ SYSTEM UPDATE ]</span> <a target="_blank" rel="noopener noreferrer" href="https://github.com/autobase-tech/autobase/releases/tag/2.10.0">Autobase 2.10 released -&gt;</a>',
         backgroundColor: 'rgb(255 92 40 / 15%)',
         isCloseable: true,
       },
+      */
       navbar: {
         hideOnScroll: true,
         title: 'autobase',
